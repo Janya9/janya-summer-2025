942b0cf0f643d67c8ff22db93c03e60252549af8
Janya <janya.9546@gmail.com>
2025-05-31 14:52:06 -0700

    added javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = "Janya";
+let age = "14";
+let likesSushi = true;
+
+function greeting(name) {
+    console.log("Hello", name);
+}
+
+greeting("Janya");
+
+let newName= document.getElementById("greeting").value;
